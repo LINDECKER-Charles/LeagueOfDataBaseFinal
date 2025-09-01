fd385a3f0f6a9c29fb187f28b98cd66c0c2a0f8a
LINDECKER Charles <92403010+LINDECKER-Charles@users.noreply.github.com>
2025-09-01 15:59:42 +0200

    Premier test du système de traduction

---
+++ app/src/EventSubscriber/DomainLocaleSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class DomainLocaleSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $host = $request->getHost();
+
+        if (str_ends_with($host, '.fr')) {
+            $request->setLocale('fr');
+        } else {
+            $request->setLocale('en');
+        }
+    }
+}
